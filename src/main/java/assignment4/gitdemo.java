ad971a2e938ed30d5ba96f9c5b8add5b24d972af
saitejagovindaram <saitejasai57@gmail.com>
2021-07-27 18:35:16 +0530

    added new print statement in gitdemo

--- src/main/java/assignment4/gitdemo.java
+++ src/main/java/assignment4/gitdemo.java
@@ -5,5 +5,7 @@ public class gitdemo {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hai");
+        System.out.println("hello");
+        System.out.println("what's happening");
     }
 }
